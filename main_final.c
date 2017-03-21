ee77acff7a0b6d70793ca1df39b8ba20fe4709c9
alex-surface <alexjmck@shaw.ca>
2017-03-21 15:19:58 -0400

    Added functions aections and defined them with void. Added part of initial clap while loop.

--- main_final.c
+++ main_final.c
@@ -6,13 +6,34 @@
 
 */
 
+//Colour detections
+void greenDownhill();
+void blueUphill();
+void redLoop();
+
+//Turn functions
+void turnLeft();
+void turnRight();
+
+//Face Functions
 void happyFace();
 void sadFace();
 void readyFace();
 
+//Spiral Function
+void spiral();
+
+
 task main()
 {
 	int count=0;
+	int colourCode;
+	
+	while(clap<50)
+	{
+		readyface();
+		
+	}
 	
 	while(count<2000)
 	{
@@ -23,19 +44,51 @@ task main()
 
 
 
+/* Colour Functions */
+
+void greenDownhill(){
+	
+}
+
+void blueUphill(){
+	
+}
+
+void redLoop(){
+	
+}
+
+
+/* Turn Functions */
+
+void turnLeft(){
+	
+}
+
+void turnRight(){
+	
+}
+
+
 /* Face Functions */
 void happyFace() {
-
-
-
+	
+	
+	
 }
-void sadFace(){
-
-
-
+void dizzyFace(){
+	
+	
+	
 }
 void readyFace(){
+	
+	
+	
+}
 
+/* Spiral */
 
-
+void spiral(){
+	
 }
