fa918c278133aa052a9760467dd4c10d6d7747b0
alex-surface <alexjmck@shaw.ca>
2017-03-21 11:31:46 -0400

    Added file and labeled face functions

---
+++ main_final.c
@@ -0,0 +1,41 @@
+
+
+/* Final project.
+   Authors: Nataniel, Alexander, Raymond.
+   APSC 142
+
+*/
+
+void happyFace();
+void sadFace();
+void readyFace();
+
+task main()
+{
+	int count=0;
+	
+	while(count<2000)
+	{
+		count = count + 1;
+		wait1Msec(2);
+	}
+}
+
+
+
+/* Face Functions */
+void happyFace() {
+
+
+
+}
+void sadFace(){
+
+
+
+}
+void readyFace(){
+
+
+
+}
