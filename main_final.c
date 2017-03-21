e0400d93b4248fcbd5fc335faee477c7e9469dd3
alex-surface <alexjmck@shaw.ca>
2017-03-21 19:08:08 -0400

    Incorperated dizzy spiral code from DizzyEyes.c into spiral(); function. Some of the task main was commented out to allow testing.

--- main_final.c
+++ main_final.c
@@ -17,19 +17,28 @@ void turnRight();
 
 //Face Functions
 void happyFace();
-void sadFace();
+void dizzyFace();
 void readyFace();
 
 //Spiral Function
 void spiral();
 
+//Dizzy eyes declarations
+float xAngle, yAngle;
+int X,X1,Y1,Y;
+float xRadius, yRadius;
+bool work = true;
+
 
 task main()
 {
-	int count=0;
-	int colourCode;
+	//	int count=0;
+	//	int colourCode;
 	
-	while(clap<50)
+	spiral();
+	wait1Msec(100);
+	
+/*	while(clap<50)
 	{
 		readyface();
 		
@@ -40,6 +49,7 @@ task main()
 		count = count + 1;
 		wait1Msec(2);
 	}
+*/	
 }
 
 
@@ -72,8 +82,8 @@ void turnRight(){
 
 /* Face Functions */
 void happyFace() {
-	drawBmpfile(0, 127, "Big smile");
-	sleep(2000);
+		drawBmpfile(0, 127, "Big smile");
+		sleep(2000);
 	
 	
 }
@@ -83,14 +93,72 @@ void dizzyFace(){
 	
 }
 void readyFace(){
-	drawBmpfile(0, 127, "Black eye");
-	sleep(2000);	
+		drawBmpfile(0, 127, "Black eye");
+		sleep(2000);	
 	
 	
 }
 
 /* Spiral */
 
-void spiral(){
+void spiral()
+{	
+	eraseDisplay();
+	while (work)
+	{
+		// Draw the spiral
+		xRadius = 32;
+		yRadius = 32;
+		xAngle = 0;
+		yAngle = 0;
+		while(xRadius > 0)
+		{
+			float xSin1,x1Sin1;
+			float yCos1,y1Cos1;
+			float xSin2,x1Sin2;
+			float yCos2,y1Cos2;
+			float fRadians,f1Radians;
+			
+			fRadians = xAngle * PI /(float) 180.0;
+			f1Radians = yAngle * PI /(float) 180.0;
+			xSin1 = sin(f1Radians);
+			x1Sin1 = sin(fRadians);
+			yCos1 = cos(fRadians);
+			y1Cos1 = cos(f1Radians);
+			xSin2 = sinDegrees(xAngle);
+			x1Sin2 = sinDegrees(yAngle);
+			yCos2 = cosDegrees(xAngle);
+			y1Cos2 = cosDegrees(yAngle);
+			X = sinDegrees(xAngle) * xRadius;
+			X1 = sinDegrees(yAngle) * yRadius;
+			Y = cosDegrees(xAngle) * xRadius;
+			Y1 = cosDegrees(yAngle) * yRadius;
+			setPixel(X + 40, Y + 72);
+			setPixel(X1 + 120, Y1 + 72);
+			wait1Msec(1);
+			xRadius -=.02;
+			yRadius -=.02;
+			++xAngle;
+			++yAngle;
+		}
+		
+		// Erase the spiral
+		while(xRadius <= 32)
+		{
+			X = sinDegrees(xAngle)  * xRadius;
+			X1 = sinDegrees(yAngle)  * yRadius;
+			Y = cosDegrees(xAngle) * xRadius;
+			Y1 = cosDegrees(yAngle) * yRadius;
+			clearPixel(X + 40, Y + 72);
+			clearPixel(X1 + 120, Y1 + 72);
+			wait1Msec(1);
+			xRadius += .02;
+			yRadius += .02;
+			--xAngle;
+			--yAngle;
+			work = false;
+		}
+		
+	}
 	
 }
